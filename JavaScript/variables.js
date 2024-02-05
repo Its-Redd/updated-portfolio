397ad63254104c886443fe278cee89536739ef7e
Redd <piihlsonyt@gmail.com>
2024-02-05 12:27:21 +0100

    I forgor 💀

---
+++ JavaScript/variables.js
@@ -0,0 +1,29 @@
+use strict;
+
+let greeting = 'Hej verden';
+console.log('Opgave 1: ' + greeting);
+
+let firstName = 'John';
+let middleName = 'F.';
+let lastName = 'Kennedy';
+let fullName = firstName + ' ' + middleName + ' ' + lastName;
+console.log('Opgave 2: ' + fullName);
+
+let salary = 5000;
+console.log('Opgave 3: ' + salary);
+
+let calculate = (5 + 5) * 3;
+console.log('Opgave 4: ' + calculate);
+
+let deci1 = 2.25;
+let deci2 = 3.5;
+let sum2 = Math.ceil(deci1 + deci2);
+console.log('Opgave 5: ' + sum2);
+
+let deci3 = 2.55;
+let deci4 = 1.44;
+let roundDown = Math.floor(deci3 + deci4);
+console.log('Opgave 6: ' + roundDown);
+
+let squareRoot = Math.sqrt(65);
+console.log('Opgave 7: ' + squareRoot);
