5ee4fbdb37614ef3357eb71a70fcfc52ebab9fcb
Redd <piihlsonyt@gmail.com>
2024-02-05 13:07:18 +0100

    Documentation

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,29 +1,36 @@
 'use strict';
 
+// Opgave 1
 let greeting = 'Hej verden';
-console.log('Opgave 1: ' + greeting);
+console.log('Opgave 1: ' + greeting); // Hej verden
 
+// Opgave 2
 let firstName = 'John';
 let middleName = 'F.';
 let lastName = 'Kennedy';
 let fullName = firstName + ' ' + middleName + ' ' + lastName;
-console.log('Opgave 2: ' + fullName);
+console.log('Opgave 2: ' + fullName); // John F. Kennedy
 
+// Opgave 3
 let salary = 5000;
-console.log('Opgave 3: ' + salary);
+console.log('Opgave 3: ' + salary); // 5000
 
+// Opgave 4
 let calculate = (5 + 5) * 3;
-console.log('Opgave 4: ' + calculate);
+console.log('Opgave 4: ' + calculate); // 30
 
+// Opgave 5
 let deci1 = 2.25;
 let deci2 = 3.5;
 let sum2 = Math.ceil(deci1 + deci2);
-console.log('Opgave 5: ' + sum2);
+console.log('Opgave 5: ' + sum2); // 6
 
+// Opgave 6
 let deci3 = 2.55;
 let deci4 = 1.44;
 let roundDown = Math.floor(deci3 + deci4);
-console.log('Opgave 6: ' + roundDown);
+console.log('Opgave 6: ' + roundDown); // 3
 
+// Opgave 7
 let squareRoot = Math.sqrt(65);
-console.log('Opgave 7: ' + squareRoot);
+console.log('Opgave 7: ' + squareRoot); // 8.06225774829855
